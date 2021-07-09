6f1876dcda75676dfe577406b824ec44df2e2bf4
k456 <katie456838@gmail.com>
2021-07-08 17:06:17 -0700

    jstask4step4

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+let form = document.getElementById("new_task_submit")
 const myTask = new TaskManager();
 myTask.addTask('Water cactus', '2 years old', 'Mom', 'Yesterday');
 console.log(myTask.tasks);
+form.addEventListener("submit", function(event){
+    console.log(form.elements[0].value);
+    event.preventDefault()
+});
