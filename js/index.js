a04afb2e828bcf4cfbbb56112b5e90b757d5bce2
Evan Lange <evanmlange@gmail.com>
2021-08-05 20:49:34 -0700

    idek what has changed

--- js/index.js
+++ js/index.js
@@ -1,26 +1,31 @@
 // Variables and HTML elements
-const myTasks = new TaskManager();
+const taskManager = new TaskManager();
 const form = document.getElementById("new_task_form");
 const taskList = document.querySelector("#task-list");
 
 // Create new task object on submit
-form.addEventListener('submit', function(event){
-    // Call addTask with form inputs
-    myTasks.addTask(
-        form.elements[0].value,
-        form.elements[1].value,
-        form.elements[2].value,
-        form.elements[3].value
-    )
-    // call the TaskManager's render method.
-    myTasks.render();
-    event.preventDefault();
-    form.reset();
+form.addEventListener("submit", function (event) {
+  // Call addTask with form inputs
+  taskManager.addTask(
+    form.elements[0].value,
+    form.elements[1].value,
+    form.elements[2].value,
+    form.elements[3].value
+  );
+  // call the TaskManager's render method.
+  taskManager.render();
+  event.preventDefault();
+  form.reset();
 });
 
+taskList.addEventListener("click", function (event) {
+  // Get current task card
+  if (event.target.classList.contains("done-button")) {
+    parentTask =
+      event.target.parentNode.parentNode.parentNode.parentNode.parentNode;
+  }
 
-taskList.addEventListener('click', function(event){
-    if (event.target.classList.contains('done-button')) {
-        parentTask = event.target.parentNode.parentNode.parentNode.parentNode.parentNode;
-    }
-})
+  // Get current parentTask's id from DOM
+  taskId = Number(parentTask.dataset.task);
+  task = taskManager.getTaskById(taskId);
+});
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,6 +1,6 @@
 const createTaskHtml = (name, description, assignedTo, dueDate, status, id) => {
-    const html = `
-        <div class="col data-task-${id}">
+  const html = `
+        <div class="col" data-task="${id}">
             <div class="card shadow-sm">
                 <img src="https://picsum.photos/400" alt="" width="100%" aria-label="Placeholder: Thumbnail">
                 <div class="card-body">
@@ -21,48 +21,68 @@ const createTaskHtml = (name, description, assignedTo, dueDate, status, id) => {
             </div>
         </div>
     `;
-    return html;
-}
+  return html;
+};
 
 class TaskManager {
-    constructor(currentId = 0) {
-        this.tasks = [];
-        this.currentId = currentId;
+  constructor(currentId = 0) {
+    this.tasks = [];
+    this.currentId = currentId;
+  }
+
+  addTask(name, description, assignedTo, dueDate, status = "TODO") {
+    this.tasks.push({
+      id: this.currentId,
+      name,
+      description,
+      assignedTo,
+      dueDate,
+      status,
+    });
+    this.currentId++;
+  }
+
+  render() {
+    const tasksHtmlList = [];
+
+    for (let task of this.tasks) {
+      // Format Date
+      let date = new Date(task.dueDate);
+      let formattedDate = `${date.getUTCMonth()}/${date.getUTCDate()}/${date.getUTCFullYear()}`;
+      console.log(formattedDate);
+
+      // Create a taskHtml variable to store the HTML of the current task, by calling the createTaskHtml function and using the properties of the current task, as well as the new formattedDate variable for the parameters.
+      let taskHtml = createTaskHtml(
+        task.name,
+        task.description,
+        task.assignedTo,
+        formattedDate,
+        task.status,
+        task.id
+      );
+
+      // push the taskHtml into the tasksHtmlList array.
+      tasksHtmlList.push(taskHtml);
     }
 
-    addTask(name, description, assignedTo, dueDate, status = TODO) {
-        this.currentId++;
-        this.tasks.push({
-            id: this.currentId,
-            name,
-            description,
-            assignedTo,
-            dueDate,
-            status,
-        });
+    // After looping through each task, create a tasksHtml variable, set the variable to a string of HTML of all the tasks by joining the tasksHtmlList array together, separating each task's html with a newline.
+    let tasksHtml = tasksHtmlList.join("\n");
+
+    //Select the tasks list element and set its innerHTML to the tasksHtml.
+    document.getElementById("task-list").innerHTML = tasksHtml;
+  }
+
+  getTaskById(taskId) {
+    // variables
+    let foundTask;
+
+    // Loop over tasks, comparing current task's id with taskId
+    for (let task of this.tasks) {
+      if (task.id === taskId) {
+        foundTask = task;
+      }
     }
 
-    render() {
-        const tasksHtmlList = [];
-
-        for (let task of this.tasks) {
-            // Format Date
-            let date = new Date(task.dueDate);
-            let formattedDate = `${date.getUTCMonth()}/${date.getUTCDate()}/${date.getUTCFullYear()}`;
-            console.log(formattedDate);
-            
-            // Create a taskHtml variable to store the HTML of the current task, by calling the createTaskHtml function and using the properties of the current task, as well as the new formattedDate variable for the parameters.
-            let taskHtml = createTaskHtml(task.name, task.description, task.assignedTo, formattedDate, task.status, task.id);
-
-            // push the taskHtml into the tasksHtmlList array.
-            tasksHtmlList.push(taskHtml);
-        }
-
-        // After looping through each task, create a tasksHtml variable, set the variable to a string of HTML of all the tasks by joining the tasksHtmlList array together, separating each task's html with a newline.
-        let tasksHtml = tasksHtmlList.join('\n');
-        
-        //Select the tasks list element and set its innerHTML to the tasksHtml.
-        document.getElementById('task-list').innerHTML = tasksHtml;
-
-    }
+    return foundTask;
+  }
 }
